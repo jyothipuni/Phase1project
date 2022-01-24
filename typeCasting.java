e918b9b15c7925cf1d038554e9930f74e8890d4e
jyothipuni <98212191+jyothipuni@users.noreply.github.com>
2022-01-24 08:53:27 +0530

    Add files via upload

---
+++ typeCasting.java
@@ -0,0 +1,15 @@
+package firstProgram;
+
+public class typeCasting
+{
+
+	public static void main(String[] args) 
+	{
+		char a='B';
+		int b=a;
+		System.out.println("The value of b: "+b);
+		float c=b;
+		System.out.println("The value of c: "+c);
+	}
+
+}
