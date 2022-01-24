3505634dd21fc4f8bb1ca2a78096a53762847084
jyothipuni <98212191+jyothipuni@users.noreply.github.com>
2022-01-24 08:55:01 +0530

    Add files via upload

---
+++ typeCasting2.java
@@ -0,0 +1,13 @@
+package secondProgram;
+
+public class typeCasting2 
+{
+
+	public static void main(String[] args) 
+	{
+	double a=54.5;
+	int b=(int)a;
+	System.out.println("The value of b: "+b);
+	}
+
+}
